619a0a90084dca3b56c17f9797d9395bde982c65
Jonas Luebbers <jluebbers@squarespace.com>
2019-01-09 17:45:33 -0500

    Move some stuff around and create package.json

---
+++ source/template.js
@@ -0,0 +1,12 @@
+const html = require('nanohtml')
+
+module.exports = (content) => {
+    return html`
+        <html>
+            <head>
+            </head>
+            <body>
+            </body>
+        </html>
+    `
+}
