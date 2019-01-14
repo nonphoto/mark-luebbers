dbce08eb506945fbe49c196ca286a5d33d3eec6b
Jonas Luebbers <jluebbers@squarespace.com>
2019-01-14 11:41:48 -0500

    Strip poem slug from filename

--- build.js
+++ build.js
@@ -5,25 +5,21 @@ const YAML = require('yaml')
 const markdown = require('markdown').markdown
 const template = require('./source/template')
 
-const content = {}
-
 const files = process.argv.slice(2)
 const promises = files.map((file) => {
     return new Promise((resolve) => {
         fs.readFile(file, (error, data) => {
             if (error) throw error
-            resolve(data)
+            const parsedData = data.toString('utf8').match(/---([\s\S]*)---([\s\S]*)/m)
+            const poem = YAML.parse(parsedData[1])
+            poem.body = markdown.toHTML(parsedData[2])
+            poem.slug = file.match(/([\w\d\-]*)\.md$/)[1]
+            resolve(poem)
         })
     })
 })
 
-Promise.all(promises).then((chunks) => {
-    content.poems = chunks.map((chunk) => {
-        const parsedChunk = chunk.toString('utf8').match(/---((.|\n)*)---((.|\n)*)/m)
-        const data = YAML.parse(parsedChunk[1])
-        data.body = markdown.toHTML(parsedChunk[3])
-        return data
-    })
-
+Promise.all(promises).then((poems) => {
+    const content = { poems }
     process.stdout.write(template(content).toString())
 })
