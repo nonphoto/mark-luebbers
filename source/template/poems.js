4931561813b807fd8735647d38f1bfb989770ca6
Jonas Luebbers <jluebbers@squarespace.com>
2019-01-14 12:30:44 -0500

    Fix preformatted not applying correctly

--- source/template/poems.js
+++ source/template/poems.js
@@ -3,10 +3,10 @@ const html = require('nanohtml')
 module.exports = (content) => {
     return content.poems.map((poem) => {
         return html`
-            <section id="${poem.slug}" class="poem ${poem.preformatted ? 'preformatted' : ''}">
+            <section id="${poem.slug}" class="poem">
                 <div class="sticky-container">
                     <div class="poem-title sticky-element"><h3>${poem.title}</h3></div>
-                    <div class="poem-content">${html(poem.body)}</div>
+                    <div class="poem-content ${poem.preformatted ? 'preformatted' : ''}">${html(poem.body)}</div>
                 </div>
             </section>
         `
