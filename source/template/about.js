8c7f76aec829ffa990a0e2aaee84e36616b196fc
Jonas Luebbers <jluebbers@squarespace.com>
2019-01-14 12:52:17 -0500

    Add contact

--- source/template/about.js
+++ source/template/about.js
@@ -10,7 +10,7 @@ module.exports = () => {
             <p>
                 “Like the subjects of these poems, I’ve lived most of my life on the border between the natural and ‘manufactured’ worlds, as I suppose many people do today, and find it a confusing, frustrating and fascinating way to live.”
             </p>
-            <p class="copyright">&copy; 2019 Mark Luebbers</p>
+            <p class="copyright"><a href="mailto:mark@luebbers.cc">Contact</a> &bull; <a href="/static/admin">Sign In</a> &bull; &copy; 2019 Mark Luebbers</p>
         </section>
     `
 }
